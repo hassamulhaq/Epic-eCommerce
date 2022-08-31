bdd2720b2679381a8b5920e358c1a7824aa597ee
hassamulhaq <developer.hassam@gmail.com>
2022-08-31 05:58:52 +0500

    products api route created

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use \App\Http\Controllers\ProductsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::group(['prefix' => 'api/v1', 'middleware' => 'auth:sanctum'], function() {
+    Route::get('/products', [ProductsController::class, 'index'])->name('products.index');
 });
