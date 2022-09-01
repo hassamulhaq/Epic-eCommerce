c9d701c94f2b291f549ad3642655001362952a52
hassamulhaq <developer.hassam@gmail.com>
2022-09-01 06:19:07 +0500

    tailwincss applied on <select>

--- resources/views/menu/_particles/chose_menu.blade.php
+++ resources/views/menu/_particles/chose_menu.blade.php
@@ -1,7 +1,7 @@
 <form action="" method="get">
     <div class="flex gap-3 items-center">
         <label>
-            <select class="border-slate-200 hover--border-slate-300 g_" name="selected_menu">
+            <select class="w-full w-40 p-1.5 bg-gray-50 border border-gray-300 text-gray-700 text-sm rounded-lg focus:ring-indigo-500 focus:border-indigo-500 block dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-indigo-500" name="selected_menu">
                 <option value="" disabled selected>-- chose --</option>
                 @forelse($onlyMenus as $menu)
                     <option value="{{ $menu['id'] }}" {{ ($menu['id'] == Request::get('selected_menu')) ? 'selected' : '' }}>
