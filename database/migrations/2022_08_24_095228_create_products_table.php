68f1b879cd8bf70b66efd9cdc6072cfda8952e82
hassamulhaq <developer.hassam@gmail.com>
2022-09-14 15:12:47 +0500

    Foregin tag_id removed in products table

--- database/migrations/2022_08_24_095228_create_products_table.php
+++ database/migrations/2022_08_24_095228_create_products_table.php
@@ -13,8 +13,8 @@ return new class extends Migration {
             $table->string('slug')->nullable();
             $table->string('short_description', 1000)->nullable();
             $table->foreignId('category_id')->nullable()->constrained('categories')->nullOnDelete();
-            $table->foreignId('tag_id')->nullable()->constrained('tags')->nullOnDelete();
-            //$table->string('tags', 500)->nullable();
+            //$table->foreignId('tag_id')->nullable()->constrained('tags')->nullOnDelete();
+            $table->string('tags', 500)->nullable();
             $table->integer('length')->nullable();
             $table->integer('width')->nullable();
             $table->integer('height')->nullable();
