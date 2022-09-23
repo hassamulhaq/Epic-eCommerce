58c00943791fa968489026c9bba4ed745b25f05d
hassamulhaq <developer.hassam@gmail.com>
2022-09-24 03:14:02 +0500

    change name field to title

--- resources/views/commerce/products/create.blade.php
+++ resources/views/commerce/products/create.blade.php
@@ -31,7 +31,7 @@
                     <select name="collections[]" id="collections" multiple="multiple" class="select2 js-choices-multiple w-full p-1.5 bg-gray-50 border border-gray-300 text-gray-700 text-sm rounded-lg focus:ring-indigo-500 focus:border-indigo-500 block dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-indigo-500 dark:focus:border-indigo-500">
                         @if($collections)
                             @foreach($collections as $collection)
-                                <option value="{{ $collection->id }}">{{ $collection->name }}</option>
+                                <option value="{{ $collection->id }}">{{ $collection->title }}</option>
                             @endforeach
                         @endif
                     </select>
