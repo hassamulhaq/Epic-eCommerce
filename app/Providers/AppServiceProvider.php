e988b1c621407ce05ca8571bd9da6399e1c91552
hassamulhaq <developer.hassam@gmail.com>
2022-10-18 05:12:32 +0500

    prevent lazyload strict mode added

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
@@ -24,7 +25,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+
         Schema::defaultStringLength(191);
+        Model::preventLazyLoading(true);
     }
 }
