b42032b4161fd296fa2876f81f19918b063af42b
hassamulhaq <developer.hassam@gmail.com>
2022-10-23 10:57:01 +0500

    Fix UI cart view

--- resources/views/components/cart-icon-dropdown.blade.php
+++ resources/views/components/cart-icon-dropdown.blade.php
@@ -22,20 +22,17 @@ $cartItems => relations:
     <div class="block py-2 px-4 font-medium text-center text-gray-700 bg-gray-50 dark:bg-gray-800 dark:text-white">
         Cart Items ({{ $cartItemsCount }})
     </div>
-    <div class="divide-y divide-gray-100 dark:divide-gray-700">
+    <div class="divide-y divide-gray-100 dark:divide-gray-700 h-96 overflow-auto">
 
         @if(!is_null($cart))
             @foreach($cart->CartItemsWithProduct as $cartItemWithProduct)
-                <a href="{{ route('products.product:slug', $cartItemWithProduct->product->productFlat->slug) }}" class="flex py-3 px-4 hover:bg-gray-100 dark:hover:bg-gray-700">
+                <a href="{{ route('products.product:slug', $cartItemWithProduct->product->productFlat->slug) }}" class="flex py-3 px-2 hover:bg-gray-100 dark:hover:bg-gray-700">
                     <div class="flex-shrink-0">
                         @if(!is_null($cartItemWithProduct->product->productFlat->getMedia('thumbnail')->first()))
                             <img class="w-11 h-11 rounded-full" src="{{ $cartItemWithProduct->product->productFlat->getMedia('thumbnail')->first()->getUrl() }}" alt="">
                         @else
                             <img class="w-11 h-11 rounded-full" src="{{ asset(\App\Helpers\Constant::PLACEHOLDER_IMAGE['path']) }}" alt="{{ \App\Helpers\Constant::PLACEHOLDER_IMAGE['alt'] }}">
                         @endif
-                        <div class="flex absolute justify-center items-center ml-6 -mt-5 w-5 h-5 bg-blue-600 rounded-full border border-white dark:border-gray-800">
-                            <svg class="w-3 h-3 text-white" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M8.707 7.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l2-2a1 1 0 00-1.414-1.414L11 7.586V3a1 1 0 10-2 0v4.586l-.293-.293z"></path><path d="M3 5a2 2 0 012-2h1a1 1 0 010 2H5v7h2l1 2h4l1-2h2V5h-1a1 1 0 110-2h1a2 2 0 012 2v10a2 2 0 01-2 2H5a2 2 0 01-2-2V5z"></path></svg>
-                        </div>
                     </div>
                     <div class="pl-3 w-full">
                         <div class="text-gray-500 text-sm mb-1.5 dark:text-gray-400">
