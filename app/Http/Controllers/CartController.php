b0058c7e313df5c715cde730116b21b34bbefb9a
hassamulhaq <developer.hassam@gmail.com>
2022-12-08 23:53:07 +0500

    Write query to get cart with cart_items using groupBy

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -12,6 +12,7 @@ use App\Models\ProductFlat;
 use App\Traits\UserHelperTrait;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use phpDocumentor\Reflection\Types\False_;
 
 class CartController extends Controller
@@ -28,11 +29,49 @@ class CartController extends Controller
     public function index()
     {
         $userId = $this->getUserId();
-        $cart = Cart::with('CartDistinctItemsWithProduct')
-            ->whereUserId($userId)
-            ->whereIsActive(true)
-            ->whereIsGuest(is_null($userId))
-            ->first();
+//        $cart = Cart::with('CartDistinctItemsWithProduct')
+//            ->whereUserId($userId)
+//            ->whereIsActive(true)
+//            ->whereIsGuest(is_null($userId))
+//            ->first();
+
+//        $cart = DB::table('cart_items')
+//            ->selectRaw('
+//                product_id,
+//                SUM(quantity) AS item_quantity,
+//                SUM(total_weight) AS item_total_weight,
+//                COUNT(item_count) AS item_count,
+//                SUM(price) AS item_price,
+//                SUM(base_price) AS item_base_price,
+//                SUM(total) AS item_total,
+//                SUM(base_total) AS item_base_total,
+//                SUM(tax_percent) AS item_tax_percent,
+//                SUM(tax_amount) AS item_tax_amount,
+//                SUM(discount_percent) AS item_discount_percent,
+//                SUM(discount_amount) AS item_discount_amount
+//                ')
+//            ->groupBy('product_id')
+//            ->get();
+
+        $cart = DB::table('cart')
+            ->join('cart_items', 'cart.id', '=', 'cart_items.cart_id', 'inner')
+                ->selectRaw('cart_items.product_id,
+                    SUM(cart_items.quantity) AS item_quantity,
+                    SUM(cart_items.total_weight) AS item_total_weight,
+                    COUNT(cart_items.item_count) AS item_count,
+                    SUM(cart_items.price) AS item_price,
+                    SUM(cart_items.base_price) AS item_base_price,
+                    SUM(cart_items.total) AS item_total,
+                    SUM(cart_items.base_total) AS item_base_total,
+                    SUM(cart_items.tax_percent) AS item_tax_percent,
+                    SUM(cart_items.tax_amount) AS item_tax_amount,
+                    SUM(cart_items.discount_percent) AS item_discount_percent,
+                    SUM(cart_items.discount_amount) AS item_discount_amount')
+                ->groupBy('cart_items.product_id')
+            ->where('cart.user_id', '=', $userId)
+            ->where('cart.is_active', '=', true)
+            ->where('cart.is_guest', '=', is_null($userId))
+            ->get();
 
         return view('frontend.cart.index', compact('cart'));
     }
