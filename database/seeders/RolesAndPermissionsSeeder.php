507a8945d6fcc40f081980556f06e5f36ad738b8
hassamulhaq <developer.hassam@gmail.com>
2022-12-19 23:31:09 +0500

    Roles And Permissions Seeder created with Role and Permissions

--- database/seeders/RolesAndPermissionsSeeder.php
+++ database/seeders/RolesAndPermissionsSeeder.php
@@ -2,10 +2,10 @@
 
 namespace Database\Seeders;
 
-use App\Helpers\UserHelper;
+
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 use Spatie\Permission\Models\Role;
-use Spatie\Permission\Models\Permission;
 
 class RolesAndPermissionsSeeder extends Seeder
 {
@@ -14,42 +14,112 @@ class RolesAndPermissionsSeeder extends Seeder
         // Reset cached roles and permissions
         app()[\Spatie\Permission\PermissionRegistrar::class]->forgetCachedPermissions();
 
-        // create permissions
-        Permission::create(['name' => 'edit products']);
-        Permission::create(['name' => 'delete products']);
-        Permission::create(['name' => 'publish products']);
-        Permission::create(['name' => 'update products']);
-        Permission::create(['name' => 'place order']);
 
-        // create roles and assign created permissions
+        $userPermission = [
+            'CAN_CREATE_USER',
+            'CAN_VIEW_USER',
+            'CAN_EDIT_USER',
+            'CAN_DELETE_USER',
+            //'HAS_ALL_USER_PERMISSION',
+        ];
+
+        $categoryPermission = [
+            'CAN_CREATE_CATEGORY',
+            'CAN_VIEW_CATEGORY',
+            'CAN_EDIT_CATEGORY',
+            'CAN_DELETE_CATEGORY',
+            //'HAS_ALL_CATEGORY_PERMISSION',
+        ];
+
+        $tagPermission = [
+            'CAN_CREATE_TAG',
+            'CAN_VIEW_TAG',
+            'CAN_EDIT_TAG',
+            'CAN_DELETE_TAG',
+            //'HAS_ALL_TAG_PERMISSION',
+        ];
+
+        $menuPermission = [
+            'CAN_CREATE_MENU',
+            'CAN_VIEW_MENU',
+            'CAN_EDIT_MENU',
+            'CAN_DELETE_MENU',
+            //'HAS_ALL_MENU_PERMISSION',
+        ];
+
+        $productPermission = [
+            'CAN_CREATE_PRODUCT',
+            'CAN_VIEW_PRODUCT',
+            'CAN_EDIT_PRODUCT',
+            'CAN_DELETE_PRODUCT',
+            //'HAS_ALL_PRODUCT_PERMISSION',
+        ];
+
+        $orderPermission = [
+            'CAN_CREATE_ORDER',
+            'CAN_VIEW_ORDER',
+            'CAN_EDIT_ORDER',
+            'CAN_DELETE_ORDER',
+            //'HAS_ALL_ORDER_PERMISSION',
+        ];
+
+        $invoicePermission = [
+            'CAN_CREATE_INVOICE',
+            'CAN_VIEW_INVOICE',
+            'CAN_EDIT_INVOICE',
+            'CAN_DELETE_INVOICE',
+            //'HAS_ALL_INVOICE_PERMISSION',
+        ];
+
+        $specialPermission = [
+            'HAS_ALL_USER_PERMISSION',
+            'HAS_ALL_CATEGORY_PERMISSION',
+            'HAS_ALL_TAG_PERMISSION',
+            'HAS_ALL_MENU_PERMISSION',
+            'HAS_ALL_PRODUCT_PERMISSION',
+            'HAS_ALL_ORDER_PERMISSION',
+            'HAS_ALL_INVOICE_PERMISSION',
+        ];
+
+        $allPermission = array_merge(
+                $userPermission,
+                $categoryPermission,
+                $tagPermission,
+                $menuPermission,
+                $productPermission,
+                $orderPermission,
+                $invoicePermission,
+                $specialPermission
+            );
+
+        $permissionsByRole = [
+            'admin' => $allPermission,
+            'customer' =>  array_merge($orderPermission, $invoicePermission),
+        ];
 
 
-//        Role::unguard();
-//
-//        $role = Role::create([
-//            'id' => UserHelper::ROLE_GUEST,
-//            'name' => 'guest'
-//        ]);
-//        $role->givePermissionTo(['place order']);
+        $insertPermissions = fn ($role) => collect($permissionsByRole[$role])
+            ->map(fn ($name) => DB::table('permissions')
+                ->insertGetId(
+                    ['name' => $name, 'guard_name' => 'web']
+                ))->toArray();
 
-        Role::create([
-            'id' => UserHelper::ROLE_SUPER_ADMIN,
-            'name' => 'super-admin'
-        ])->givePermissionTo(Permission::all());
+        $permissionIdsByRole = [
+            'admin' => $insertPermissions('admin'),
+            'customer' => $insertPermissions('customer')
+        ];
 
+        foreach ($permissionIdsByRole as $role => $permissionIds) {
+            $role = Role::whereName($role)->first();
 
-        // this can be done as separate statements
-        $role = Role::create([
-            'id' => UserHelper::ROLE_ADMIN,
-            'name' => 'admin'
-        ]);
-        $role->givePermissionTo('edit products', 'delete products', 'publish products', 'update products');
+            DB::table('role_has_permissions')
+                ->insert(
+                    collect($permissionIds)->map(fn ($id) => [
+                        'role_id' => $role->id,
+                        'permission_id' => $id
+                    ])->toArray()
+                );
+        }
 
-        // or may be done by chaining
-        $role = Role::create([
-            'id' => UserHelper::ROLE_CUSTOMER,
-            'name' => 'customer'
-        ]);
-        $role->givePermissionTo(['place order']);
     }
 }
